f340e6185dceff607f4320e708f7cc1dbf5400d7
Cleberson <pauluci.cleberson@gmail.com>
2024-02-14 14:37:10 -0300

    Refactored job struct and changed field from specific type to generic type with interface

--- prices/prices.go
+++ prices/prices.go
@@ -2,16 +2,16 @@ package prices
 
 import (
 	"example.com/price-calculator/conversion"
-	"example.com/price-calculator/filemanager"
+	"example.com/price-calculator/iomanager"
 	"fmt"
 )
 
 // TaxIncludedPriceJob - to ignore serialization of a field just put -(hyphen) after json: tag
 type TaxIncludedPriceJob struct {
-	IOManager         filemanager.FileManager `json:"-"`
-	TaxRate           float64                 `json:"tax_rate"`
-	InputPrices       []float64               `json:"input_prices"`
-	TaxIncludedPrices map[string]string       `json:"tax_included_prices"`
+	IOManager         iomanager.IOManager `json:"-"`
+	TaxRate           float64             `json:"tax_rate"`
+	InputPrices       []float64           `json:"input_prices"`
+	TaxIncludedPrices map[string]string   `json:"tax_included_prices"`
 }
 
 func (job *TaxIncludedPriceJob) LoadData() {
@@ -51,9 +51,9 @@ func (job *TaxIncludedPriceJob) Process() {
 	}
 }
 
-func NewTaxIncludedPriceJob(fm filemanager.FileManager, taxRate float64) *TaxIncludedPriceJob {
+func NewTaxIncludedPriceJob(iom iomanager.IOManager, taxRate float64) *TaxIncludedPriceJob {
 	return &TaxIncludedPriceJob{
-		IOManager: fm,
+		IOManager: iom,
 		TaxRate:   taxRate,
 	}
 }
