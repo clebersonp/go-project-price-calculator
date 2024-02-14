c14771e04ac598d24511bdadffaec5244dc2ab01
Cleberson <pauluci.cleberson@gmail.com>
2024-02-14 10:22:30 -0300

    Created the Process method for create the map with prices and their prices taxed

--- main.go
+++ main.go
@@ -1,23 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"example.com/price-calculator/prices"
+	"fmt"
+)
 
 func main() {
-	prices := []float64{10, 20, 30}
 	taxRates := []float64{0, 0.07, 0.1, 0.15}
 
-	result := make(map[float64][]float64)
+	jobs := make([]*prices.TaxIncludedPriceJob, len(taxRates))
 
-	for _, taxRate := range taxRates {
-		// len(prices) is the initial empty slots of slice
-		// because we are using the initial as the slices length
-		// we must access with its index position to avoid empty values at the beginning of slice
-		pricesTaxed := make([]float64, len(prices))
-		for priceIndex, price := range prices {
-			pricesTaxed[priceIndex] = price * (1 + taxRate)
-		}
-		result[taxRate] = pricesTaxed
+	for i, taxRate := range taxRates {
+		priceJob := prices.NewTaxIncludedPriceJob(taxRate)
+		priceJob.Process()
+		jobs[i] = priceJob
 	}
 
-	fmt.Println(result)
+	for _, job := range jobs {
+		fmt.Println(*job)
+	}
 }
--- prices/prices.go
+++ prices/prices.go
@@ -1,9 +1,18 @@
 package prices
 
+import "fmt"
+
 type TaxIncludedPriceJob struct {
 	TaxRate           float64
 	InputPrices       []float64
 	TaxIncludedPrices map[string]float64
+}
+
+func (job *TaxIncludedPriceJob) Process() {
+	job.TaxIncludedPrices = make(map[string]float64)
+	for _, price := range job.InputPrices {
+		job.TaxIncludedPrices[fmt.Sprintf("%.2f", price)] = price * (1 + job.TaxRate)
+	}
 }
 
 func NewTaxIncludedPriceJob(taxRate float64) *TaxIncludedPriceJob {
