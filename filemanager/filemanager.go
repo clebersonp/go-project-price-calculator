f69fb9adbe3597a350c2c6e0f067a08e451b6d13
Cleberson <pauluci.cleberson@gmail.com>
2024-02-14 13:54:45 -0300

    Refactored filemanger to be a struct and transformed all function into its methods

--- filemanager/filemanager.go
+++ filemanager/filemanager.go
@@ -7,8 +7,22 @@ import (
 	"os"
 )
 
-func ReadLines(path string) ([]string, error) {
-	file, err := os.Open(path)
+type FileManager struct {
+	InputFilePath  string `json:"input_file_path"`
+	OutputFilePath string `json:"output_file_path"`
+}
+
+// New - in this constructor we don't need to return a pointer because it's so smaller and doesn't manner
+func New(inputFilePath, outputFilePath string) FileManager {
+	return FileManager{
+		InputFilePath:  inputFilePath,
+		OutputFilePath: outputFilePath,
+	}
+}
+
+// ReadLines - it does not need to receive a file manager pointer because we don't need to change its values, just read
+func (fm FileManager) ReadLines() ([]string, error) {
+	file, err := os.Open(fm.InputFilePath)
 	if err != nil {
 		return nil, errors.New("failed to open file")
 	}
@@ -32,8 +46,9 @@ func ReadLines(path string) ([]string, error) {
 	return lines, nil
 }
 
-func WriteJSON(path string, data any) error {
-	file, err := os.Create(path)
+// WriteResult - it does not need to receive a file manager pointer because we don't need to change its values, just read
+func (fm FileManager) WriteResult(data any) error {
+	file, err := os.Create(fm.OutputFilePath)
 	if err != nil {
 		return errors.New("failed to create file")
 	}
